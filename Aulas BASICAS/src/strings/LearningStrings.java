38fd89b17cab49878dfdcd97da6b8910a2440c34
Joao Victor Saint Just <joaosaintjust@hotmail.com>
2024-02-05 22:24:58 -0300

    Strings

---
+++ Aulas BASICAS/src/strings/LearningStrings.java
@@ -0,0 +1,15 @@
+package strings;
+
+public class LearningStrings {
+  public static void main(String[] args) {
+
+    char letra = 'a'; // ele só aceita um caractere e usa como base numeros da tabela ASCII
+    System.out.println(letra);
+
+    String mensagem = "Hello World!"; // ele aceita uma sequencia de caracteres
+    System.out.println(mensagem);
+
+    mensagem = mensagem + 2024; // ele aceita uma sequencia de caracteres
+    System.err.println(mensagem);
+  }
+}
